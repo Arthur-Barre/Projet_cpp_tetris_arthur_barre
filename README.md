ba51c1d249e272a32119bf0c93bc0407b53bafa5
Arthur-Barre <arthur.barre@etu.minesparis.psl.eu>
2025-01-14 16:01:21 +0100

    Version de base 1

--- README.md
+++ README.md
@@ -1,1 +1,5 @@
 # Projet_cpp_tetris_arthur_barre
+
+projet d'informatique pour février 2025
+
+But: créer un tétris fonctionnel
---
+++ Tetris_v1.cpp
@@ -0,0 +1,10 @@
+#include <vector>
+#include <iostream>
+#include <SFML/Graphics.hpp>
+#include <SFML/Window.hpp>
+#include <SFML/Audio.hpp>
+
+int main(){
+
+    return 0;
+}
